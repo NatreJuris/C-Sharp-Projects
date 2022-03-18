508e4204ccb9388df89e383a43d8d5a71b41f3c3
NatreJuris <95266939+NatreJuris@users.noreply.github.com>
2022-03-18 10:35:00 -0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //console.writeline("the tech academy");
+            //console.writeline("student daily report");      //questeions for students daily report.
+            //console.writeline("what is your name?");
+            //string yourname = console.readline();
+            //console.writeline("what course are you on?");
+            //string course = console.readline();
+            //console.writeline("what page number?");
+            //string onpage = console.readline();
+            //console.writeline("do you need help with anything ? please answer “true” or “false.");
+            //string onhelp = console.readline();
+            //console.writeline("were there any positive experiences you’d like to share? please give specifics.");
+            //string experience = console.readline();
+            //console.writeline("is there any other feedback you’d like to provide? please be specific.");
+            //string feedback = console.readline();
+            //console.writeline("how many hours did you study today?");
+            //string onhours = console.readline();
+            //console.writeline("thank you for your answers. an instructor will respond to this shortly. have a great day!");
+            //string thanks = console.readline();
+            
+             
+        }
+    }
+}
