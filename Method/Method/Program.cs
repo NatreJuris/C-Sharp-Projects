e99d67942645f0847b5986cc6d28960b37d93bd8
NatreJuris <95266939+NatreJuris@users.noreply.github.com>
2022-04-01 00:23:48 -0700

    Add files via upload

---
+++ Method/Method/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TakesTwo add = new TakesTwo();
+            Console.WriteLine("Input two numbers, one at a time. The second number is optional");
+            int number = Convert.ToInt32(Console.ReadLine());
+            var number1 = int.TryParse(Console.ReadLine(), out int x);
+
+            int result = 0;
+            if (number1)
+            {
+                result = add.Two(number, x);
+            }
+            else
+            {
+                result = add.Two(number);
+            }
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Method/Method/TakesTwo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class TakesTwo
+    {
+        public int Two(int num, int num2 = 5)
+
+        {
+            return(num * num2);
+        }
+    }
+}
