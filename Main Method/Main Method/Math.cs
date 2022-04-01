0365d8d5493009172a8b3514eafc995a5ee52090
NatreJuris <95266939+NatreJuris@users.noreply.github.com>
2022-03-31 22:31:48 -0700

    Add files via upload

---
+++ Main Method/Main Method/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Math
+    {
+        public int Method(int num)
+        {
+            return num * 2;
+        }
+        public double Method(double num)
+        {
+            return num / 2;
+        }
+        public string Method( string x)
+        {
+            int num2 = Int32.Parse(x);
+            int o = num2 + 3;
+            x = Convert.ToString(o);
+            return Convert.ToString(x);
+        }
+
+    }   
+}
---
+++ Main Method/Main Method/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math add = new Math();
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(add.Method(number));
+            Console.ReadLine();
+
+            Math add1 = new Math();
+            double number1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(add1.Method(number1));
+            Console.ReadLine();
+
+            Math add2 = new Math();
+            string number2 = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(add2.Method(number2));
+            Console.ReadLine();
+        }
+    }
+}
