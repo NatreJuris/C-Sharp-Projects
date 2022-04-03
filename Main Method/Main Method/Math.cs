4c759a49775603c91c24bf0c0ab540132d38cbdd
NatreJuris <95266939+NatreJuris@users.noreply.github.com>
2022-04-03 16:36:06 -0700

    Add files via upload

--- Main Method/Main Method/Math.cs
+++ Main Method/Main Method/Math.cs
@@ -12,11 +12,11 @@ namespace Main_Method
         {
             return num * 2;
         }
-        public decimal Method(decimal num)
+        public int Method(decimal num1)
         {
-            return num / 2;
+            return Convert.ToInt32(num1 / 2);
         }
-        public string Method( string x)
+        public int Method(string x)
         {
             int result = 0;
             try
@@ -24,14 +24,14 @@ namespace Main_Method
                 int num2 = Int32.Parse(x);
                 int o = num2 + 3;
                 x = Convert.ToString(o);
-                return Convert.ToString(x);
+                return Convert.ToInt32(x);
             }
             catch (FormatException)
             {
                 Console.WriteLine("You have entered an invalid numeric value. Feel free to restart and try again.");
                 result = 0;
             }
-            return Convert.ToString(result);
+            return Convert.ToInt32(result);
         }
 
     }   
