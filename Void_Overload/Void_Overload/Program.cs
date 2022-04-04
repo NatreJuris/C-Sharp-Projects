3a0b3faa0790c92f32ec166913470d55d6befd05
NatreJuris <95266939+NatreJuris@users.noreply.github.com>
2022-04-03 23:29:49 -0700

    Add files via upload

---
+++ Void_Overload/Void_Overload/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Overload
+{
+    static class Program
+    {
+        public static int num { get; set; }
+        
+
+        public static void Main(string[] args)
+        {
+            Purple purple = new Purple();
+            Console.WriteLine("Enter a number to be divided in two.");
+            purple.First(num);
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number/2);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Void_Overload/Void_Overload/Purple.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Overload
+{
+    class Purple
+    {
+  
+        public static int num { get; set; }
+
+
+
+        public void First(int num, out int x)
+        {
+            Console.WriteLine(num / 2);
+            x = Convert.ToInt32(Console.ReadLine());
+
+           
+        }
+        public void First(int num)
+        {
+            Console.WriteLine(num / 2);
+        }
+
+
+
+    }
+}
