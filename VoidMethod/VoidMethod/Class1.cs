5ccdef02300e2db942acc99b6004fbc8f775b159
NatreJuris <95266939+NatreJuris@users.noreply.github.com>
2022-04-04 17:08:46 -0700

    Add files via upload

---
+++ VoidMethod/VoidMethod/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    public static class Class1
+    {
+        public static void Method(int num)
+        {
+            Console.WriteLine(num / 2);
+            Console.ReadLine();
+
+        }
+        public static void Method(string h, out int x)
+        {
+            x = 5;
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static VoidMethod.Class1;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to be divided by 2.");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Method(num);
+
+        }
+    }
+}
