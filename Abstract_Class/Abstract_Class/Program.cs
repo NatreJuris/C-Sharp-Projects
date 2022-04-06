43a7e853ddc0e5a093e641d55275f1b25c4d1574
NatreJuris <95266939+NatreJuris@users.noreply.github.com>
2022-04-06 10:15:47 -0700

    Add files via upload

---
+++ Abstract_Class/Abstract_Class/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee() { firstName = " Sample ", lastName = "Student" };
+
+            emp.SayName();
+          
+
+        }
+    }
+}
