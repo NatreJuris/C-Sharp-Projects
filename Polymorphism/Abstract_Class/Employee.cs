b00d3021db4c23524deaddabc8e5bf455839828f
NatreJuris <95266939+NatreJuris@users.noreply.github.com>
2022-04-06 20:16:13 -0700

    Add files via upload

---
+++ Polymorphism/Abstract_Class/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    public class Employee : Person, IQuittable
+    {
+     
+        public override void SayName()
+        {
+            Console.WriteLine("Name:" + firstName + lastName);
+            Console.ReadLine();
+        }
+        public void Quit(Employee employee)
+        {
+            Console.WriteLine("Looks like" + firstName + lastName + " has had enouph of this place and has quit.");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Polymorphism/Abstract_Class/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee() { firstName = " Sample ", lastName = "Student" };
+
+            emp.SayName();
+
+            Person emp1 = new Employee()
+            {
+
+            };
+            emp.Quit(emp);
+        }
+    }
+}
