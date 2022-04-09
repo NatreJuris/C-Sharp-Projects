745b583907ef71553133e335d7d2b97d143fd076
NatreJuris <95266939+NatreJuris@users.noreply.github.com>
2022-04-08 23:16:11 -0700

    Add files via upload

---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a day of the week.");
+            string day = (Console.ReadLine());
+
+            try
+            {
+                WeekDays days = (WeekDays)Enum.Parse(typeof(WeekDays), day);
+
+                Console.WriteLine("Thanks");
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+
+                Console.ReadLine();
+
+            }
+
+
+        }
+    }
+}
---
+++ ParsingEnums/ParsingEnums/Week.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    class Week
+    {
+        public WeekDays Weekdays { get; set; }
+
+    }
+    public enum WeekDays
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday, 
+        sunday,
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        friday,
+        saturday,
+    }
+}
