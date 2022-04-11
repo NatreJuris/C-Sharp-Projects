923b161c3de134adc58e740b14f65382fbcd8dbe
NatreJuris <95266939+NatreJuris@users.noreply.github.com>
2022-04-10 21:59:10 -0700

    Add files via upload

---
+++ DaTim/DaTim/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaTim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            Console.WriteLine(localDate);
+            Console.ReadLine();
+            Console.WriteLine("Enter a number.");
+            double num = Double.Parse(Console.ReadLine());
+            
+            DateTime dt = localDate.AddHours(num);
+
+            Console.WriteLine(dt);
+            Console.ReadLine();
+
+          
+        }
+    }
+}
