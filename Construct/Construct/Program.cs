0d03588ad88ce1b9c92342c889ecdd769f63f1cf
NatreJuris <95266939+NatreJuris@users.noreply.github.com>
2022-04-11 02:31:58 -0700

    Add files via upload

---
+++ Construct/Construct/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construct
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const string yep = "Yep, ";
+            
+            var nope = "that's right.";
+
+            var ok = nope;
+
+            Console.WriteLine(yep);
+            Console.ReadLine();
+            Console.WriteLine(nope);
+            Console.ReadLine();
+            Console.WriteLine(yep + ok);
+            Console.ReadLine();
+        }
+    }
+}
