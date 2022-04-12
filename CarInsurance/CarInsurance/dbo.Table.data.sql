b6d3679fbbf87d3927c8c597c3cfa6b4316cb29b
NatreJuris <95266939+NatreJuris@users.noreply.github.com>
2022-04-12 00:47:23 -0700

    Add files via upload

---
+++ CarInsurance/CarInsurance/dbo.Table.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[Table] ON
+INSERT INTO [dbo].[Table] ([Id], [FirstName], [LasstName], [EmailAddress], [DateOfBirth], [CarYear], [CarMake], [CarModel], [DUI], [SpeedingTickets], [CoverageType]) VALUES (NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+SET IDENTITY_INSERT [dbo].[Table] OFF
---
+++ CarInsurance/CarInsurance/dbo.Table.sql
@@ -0,0 +1,43 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [Id]              INT NOT NULL,
+    [FirstName]       NVARCHAR (50) NOT NULL,
+    [LasstName]       NVARCHAR (50) NOT NULL,
+    [EmailAddress]    NVARCHAR (50) NOT NULL,
+    [DateOfBirth]     DATETIME      NOT NULL,
+    [CarYear]         INT           NOT NULL,
+    [CarMake]         NVARCHAR (50) NOT NULL,
+    [CarModel]        NVARCHAR (50) NOT NULL,
+    [DUI]             BIT           NOT NULL,
+    [SpeedingTickets] INT           NOT NULL,
+    [CoverageType]    BIT           NOT NULL,
+    [Quote]           MONEY         NULL IDENTITY DEFAULT {0}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
